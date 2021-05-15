ce41d1d2763538ab7238e13a8252df5e67c84887
Srujan <sai.srujan889@gmail.com>
2021-05-16 01:35:41 +0530

    Source Code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (username, password, enabled)
 SELECT 'vc', 'manage', '1'
-WHERE NOT EXISTS (SELECT * FROM users WHERE firstname='admin');
+WHERE NOT EXISTS (SELECT * FROM users WHERE username='vc');
