0b53c6c10470fc325ee02f89be3c6ee73e03b678
Srujan <sai.srujan889@gmail.com>
2021-05-16 13:51:57 +0530

    Data SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+INSERT INTO users (username, password, enabled)
+SELECT 'admin', 'manage', 'TRUE'
+WHERE NOT EXISTS (SELECT * FROM users WHERE username='admin');
 INSERT INTO authorities (username, authority)
 SELECT 'admin', 'ADMIN'
 WHERE NOT EXISTS (SELECT * FROM authorities WHERE username='admin');
